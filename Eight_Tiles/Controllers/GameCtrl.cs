76685f1d2225750e5d20070d67ed3590aad8e3c4
Darren Morby <nullpointersetc@users.noreply.github.com>
2026-04-14 12:38:33 -0400

    Add method that generates the eight-tile game board

--- Eight_Tiles/Controllers/GameCtrl.cs
+++ Eight_Tiles/Controllers/GameCtrl.cs
@@ -20,6 +20,7 @@ using ControllerBase = Microsoft.AspNetCore.Mvc.ControllerBase;
 using HttpGet = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
 using IActionResult = Microsoft.AspNetCore.Mvc.IActionResult;
 using Route = Microsoft.AspNetCore.Mvc.RouteAttribute;
+using StringBuilder = System.Text.StringBuilder;
 
 namespace NullPointersEtc.Eight_Tiles.Controllers
 {
@@ -39,41 +40,64 @@ namespace NullPointersEtc.Eight_Tiles.Controllers
     public class GameController : ControllerBase
     {
         [method: HttpGet, Route("index.html")]
-        public IActionResult Index()
+        public IActionResult IndexDotHtml()
         {
-            string htmlContent = @"
-                <!DOCTYPE html>
-                <html>
-                <head>
-                    <title>Inline HTML</title>
-                    <meta charset='UTF-8'>
-                </head>
-                <body>
-                    <h1>Hello from ASP.NET Core!</h1>
-                    <table style=""border: 1px solid;"">
-<tbody>
-<tr><td style=""border: 1px solid;"">1</td>
-<td style=""border: 1px solid;"">2</td>
-<td style=""border: 1px solid;"">3</td><tr>
-<tr><td style=""border: 1px solid;"">4</td>
-<td style=""border: 1px solid;"">5</td>
-<td style=""border: 1px solid;"">6</td><tr>
-<tr><td style=""border: 1px solid;"">7</td>
-<td style=""border: 1px solid;"">8</td>
-<td></td><tr>
-</tbody>
-</table>
-                </body>
-                </html>";
+            return this.GameDotHtml(
+                1,2,3,
+                4,5,6,
+                0,7,8);
+        }
 
+        [method: HttpGet, Route("game.html")]
+        public IActionResult GameDotHtml(
+            int t11, int t12, int t13,
+            int t21, int t22, int t23,
+            int t31, int t32, int t33)
+        {
             return new ContentResult
             {
-                Content = htmlContent,
+                Content = new StringBuilder("<!DOCTYPE html>")
+                    .AppendLine()
+                    .AppendLine("<html>")
+                    .AppendLine("<head>")
+                    .AppendLine("<title>Eight-Tiles Game</title>")
+                    .AppendLine("<meta charset=\"UTF-8\">")
+                    .AppendLine("</head>")
+                    .AppendLine("<body>")
+                    .AppendLine("<h1>Eight-Tiles Game</h1>")
+                    .AppendLine("<table style=\"border: 1px solid;\">")
+                    .AppendLine("<tbody>")
+                    .AppendLine("<tr>")
+                    .Append("<td style=\"border: 1px solid;\">")
+                    .Append(t11).AppendLine("</td>")
+                    .Append("<td style=\"border: 1px solid;\">")
+                    .Append(t12).AppendLine("</td>")
+                    .Append("<td style=\"border: 1px solid;\">")
+                    .Append(t13).AppendLine("</td>")
+                    .AppendLine("</tr>")
+                    .AppendLine("<tr>")
+                    .Append("<td style=\"border: 1px solid;\">")
+                    .Append(t21).AppendLine("</td>")
+                    .Append("<td style=\"border: 1px solid;\">")
+                    .Append(t22).AppendLine("</td>")
+                    .Append("<td style=\"border: 1px solid;\">")
+                    .Append(t23).AppendLine("</td>")
+                    .AppendLine("</tr>")
+                    .AppendLine("<tr>")
+                    .Append("<td style=\"border: 1px solid;\">")
+                    .Append(t31).AppendLine("</td>")
+                    .Append("<td style=\"border: 1px solid;\">")
+                    .Append(t32).AppendLine("</td>")
+                    .Append("<td style=\"border: 1px solid;\">")
+                    .Append(t33).AppendLine("</td>")
+                    .AppendLine("</tr>")
+                    .AppendLine("</tbody>")
+                    .AppendLine("</table>")
+                    .AppendLine("</body>")
+                    .AppendLine("</html>").ToString(),
                 ContentType = "text/html",
                 StatusCode = 200
             };
-
-
         }
     }
 }
