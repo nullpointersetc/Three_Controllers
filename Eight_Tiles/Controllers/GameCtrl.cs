a6c4bbd82eeea7ead3ac57279f4308766e0a11bd
Darren Morby <nullpointersetc@users.noreply.github.com>
2026-04-14 15:52:49 -0400

    make eight-tile game easier to access

--- Eight_Tiles/Controllers/GameCtrl.cs
+++ Eight_Tiles/Controllers/GameCtrl.cs
@@ -1,103 +1,78 @@
-/*using ApiController_a = Microsoft.AspNetCore.Mvc.ApiControllerAttribute;
-using Route_a = Microsoft.AspNetCore.Mvc.RouteAttribute;
-using HttpGet_a = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
-using HttpPost_a = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
-using HttpDelete_a = Microsoft.AspNetCore.Mvc.HttpDeleteAttribute;
-using IActionResult_t = Microsoft.AspNetCore.Mvc.IActionResult;
-using ControllerBase_t = Microsoft.AspNetCore.Mvc.ControllerBase;
-using FromBody_a = Microsoft.AspNetCore.Mvc.FromBodyAttribute;
-using OrderCreateDTO_t = NullPointersEtc.ThreeControllers.Models.OrderCreateDTO;
-using OrderDTO_t = NullPointersEtc.ThreeControllers.Models.OrderDTO;
-using Order_t = NullPointersEtc.ThreeControllers.Models.Order;
-using IOrderService_t = NullPointersEtc.ThreeControllers.Services.IOrderService;
-using Guid_t = System.Guid;
-
-*/
-using System;
 using ApiController = Microsoft.AspNetCore.Mvc.ApiControllerAttribute;
 using ContentResult = Microsoft.AspNetCore.Mvc.ContentResult;
 using ControllerBase = Microsoft.AspNetCore.Mvc.ControllerBase;
 using HttpGet = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
-using IActionResult = Microsoft.AspNetCore.Mvc.IActionResult;
+using NotNull = System.Diagnostics.CodeAnalysis.NotNullAttribute;
 using Route = Microsoft.AspNetCore.Mvc.RouteAttribute;
 using StringBuilder = System.Text.StringBuilder;
 
-namespace NullPointersEtc.Eight_Tiles.Controllers
+#pragma warning disable IDE0130
+namespace NullPointersEtc.Eight_Tiles.Controllers;
+
+[type: ApiController, Route("eighttiles")]
+public class GameController : ControllerBase
 {
-    public interface IGameService
+    [method: HttpGet, Route("index.html")]
+    [return: NotNull]
+    public ContentResult IndexDotHtml()
     {
-        IEnumerable<int> GetAll();
+        return this.GameDotHtml(
+            1,2,3,
+            4,5,6,
+            0,7,8);
     }
 
-    public class GameService : IGameService
+    [method: HttpGet, Route("game.html")]
+    [return: NotNull]
+    public ContentResult GameDotHtml(
+        int t11, int t12, int t13,
+        int t21, int t22, int t23,
+        int t31, int t32, int t33)
     {
-        public GameService() { }
-
-        public IEnumerable<int> GetAll() => [1, 2, 3, 4];
-    }
-
-    [type: ApiController, Route("eighttiles")]
-    public class GameController : ControllerBase
-    {
-        [method: HttpGet, Route("index.html")]
-        public IActionResult IndexDotHtml()
+        return new ContentResult
         {
-            return this.GameDotHtml(
-                1,2,3,
-                4,5,6,
-                0,7,8);
-        }
-
-        [method: HttpGet, Route("game.html")]
-        public IActionResult GameDotHtml(
-            int t11, int t12, int t13,
-            int t21, int t22, int t23,
-            int t31, int t32, int t33)
-        {
-            return new ContentResult
-            {
-                Content = new StringBuilder("<!DOCTYPE html>")
-                    .AppendLine()
-                    .AppendLine("<html>")
-                    .AppendLine("<head>")
-                    .AppendLine("<title>Eight-Tiles Game</title>")
-                    .AppendLine("<meta charset=\"UTF-8\">")
-                    .AppendLine("</head>")
-                    .AppendLine("<body>")
-                    .AppendLine("<h1>Eight-Tiles Game</h1>")
-                    .AppendLine("<table style=\"border: 1px solid;\">")
-                    .AppendLine("<tbody>")
-                    .AppendLine("<tr>")
-                    .Append("<td style=\"border: 1px solid;\">")
-                    .Append(t11).AppendLine("</td>")
-                    .Append("<td style=\"border: 1px solid;\">")
-                    .Append(t12).AppendLine("</td>")
-                    .Append("<td style=\"border: 1px solid;\">")
-                    .Append(t13).AppendLine("</td>")
-                    .AppendLine("</tr>")
-                    .AppendLine("<tr>")
-                    .Append("<td style=\"border: 1px solid;\">")
-                    .Append(t21).AppendLine("</td>")
-                    .Append("<td style=\"border: 1px solid;\">")
-                    .Append(t22).AppendLine("</td>")
-                    .Append("<td style=\"border: 1px solid;\">")
-                    .Append(t23).AppendLine("</td>")
-                    .AppendLine("</tr>")
-                    .AppendLine("<tr>")
-                    .Append("<td style=\"border: 1px solid;\">")
-                    .Append(t31).AppendLine("</td>")
-                    .Append("<td style=\"border: 1px solid;\">")
-                    .Append(t32).AppendLine("</td>")
-                    .Append("<td style=\"border: 1px solid;\">")
-                    .Append(t33).AppendLine("</td>")
-                    .AppendLine("</tr>")
-                    .AppendLine("</tbody>")
-                    .AppendLine("</table>")
-                    .AppendLine("</body>")
-                    .AppendLine("</html>").ToString(),
-                ContentType = "text/html",
-                StatusCode = 200
-            };
-        }
+            Content = new StringBuilder("<!DOCTYPE html>")
+                .AppendLine()
+                .AppendLine("<html>")
+                .AppendLine("<head>")
+                .AppendLine("<title>Eight-Tiles Game</title>")
+                .AppendLine("<meta charset=\"UTF-8\">")
+                .AppendLine("</head>")
+                .AppendLine("<body>")
+                .AppendLine("<h1>Eight-Tiles Game</h1>")
+                .AppendLine("<table style=\"border: 1px solid;\">")
+                .AppendLine("<tbody>")
+                .AppendLine("<tr>")
+                .Append("<td style=\"border: 1px solid;\">")
+                .Append(t11).AppendLine("</td>")
+                .Append("<td style=\"border: 1px solid;\">")
+                .Append(t12).AppendLine("</td>")
+                .Append("<td style=\"border: 1px solid;\">")
+                .Append(t13).AppendLine("</td>")
+                .AppendLine("</tr>")
+                .AppendLine("<tr>")
+                .Append("<td style=\"border: 1px solid;\">")
+                .Append(t21).AppendLine("</td>")
+                .Append("<td style=\"border: 1px solid;\">")
+                .Append(t22).AppendLine("</td>")
+                .Append("<td style=\"border: 1px solid;\">")
+                .Append(t23).AppendLine("</td>")
+                .AppendLine("</tr>")
+                .AppendLine("<tr>")
+                .Append("<td style=\"border: 1px solid;\">")
+                .Append(t31).AppendLine("</td>")
+                .Append("<td style=\"border: 1px solid;\">")
+                .Append(t32).AppendLine("</td>")
+                .Append("<td style=\"border: 1px solid;\">")
+                .Append(t33).AppendLine("</td>")
+                .AppendLine("</tr>")
+                .AppendLine("</tbody>")
+                .AppendLine("</table>")
+                .AppendLine("</body>")
+                .AppendLine("</html>").ToString(),
+            ContentType = "text/html",
+            StatusCode = 200
+        };
     }
 }
+#pragma warning restore IDE0130
--- Three_Controllers/Program.cs
+++ Three_Controllers/Program.cs
@@ -1,18 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using StatusCodes= Microsoft.AspNetCore.Http.StatusCodes;
 using IOrderService = NullPointersEtc.ThreeControllers.Services.IOrderService;
 using IProductService = NullPointersEtc.ThreeControllers.Services.IProductService;
 using IUserService = NullPointersEtc.ThreeControllers.Services.IUserService;
 using OrderService = NullPointersEtc.ThreeControllers.Services.OrderService;
 using ProductService = NullPointersEtc.ThreeControllers.Services.ProductService;
 using UserService = NullPointersEtc.ThreeControllers.Services.UserService;
-using WebApplication_t = Microsoft.AspNetCore.Builder.WebApplication;
-using WebApplicationBuilder_t = Microsoft.AspNetCore.Builder.WebApplicationBuilder;
+using WebApplication = Microsoft.AspNetCore.Builder.WebApplication;
+using WebApplicationBuilder = Microsoft.AspNetCore.Builder.WebApplicationBuilder;
 using DatabaseContext = NullPointersEtc.ThreeControllers.Data.Context;
 using IServiceScope = Microsoft.Extensions.DependencyInjection.IServiceScope;
-using IGameService = NullPointersEtc.Eight_Tiles.Controllers.IGameService;
-using GameService = NullPointersEtc.Eight_Tiles.Controllers.GameService;
+using GameController = NullPointersEtc.Eight_Tiles.Controllers.GameController;
 
 namespace NullPointersEtc.ThreeControllers;
 
@@ -20,7 +18,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        WebApplicationBuilder_t builder = WebApplication_t.CreateBuilder(args);
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddDbContext<DatabaseContext>(options =>
                   options.UseSqlServer(
@@ -30,9 +28,9 @@ public class Program
         builder.Services.AddScoped<IProductService, ProductService>();
         builder.Services.AddScoped<IOrderService, OrderService>();
         builder.Services.AddScoped<IUserService, UserService>();
-        builder.Services.AddScoped<IGameService, GameService>();
+        builder.Services.AddScoped<GameController, GameController>();
 
-        WebApplication_t app = builder.Build();
+        WebApplication app = builder.Build();
         using (IServiceScope scope = app.Services.CreateScope())
         {
             DatabaseContext context = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
@@ -47,18 +45,14 @@ public class Program
 
     public static void Index(Microsoft.AspNetCore.Http.HttpContext context)
     {
-        context.Response.StatusCode = StatusCodes.Status200OK;
-        context.Response.ContentType = "text/html";
-        context.Response.WriteAsync("<!DOCTYPE html>\n"+
-                "<html>\n"+
-                "<head>\n"+
-                    "<title>Inline HTML</title>\n"+
-                    "<meta charset='UTF-8'>\n"+
-                "</head>\n"+
-                "<body>\n"+
-                    "<h1>Hello from ASP.NET Core!</h1>\n"+
-                    "<p><a href=\"eighttiles/index.html\">Link</a></p>\n"+
-                "</body>\n"+
-                "</html>\n");
+        GameController eightTiles = new GameController();
+
+        ContentResult response = eightTiles.IndexDotHtml();
+
+        context.Response.StatusCode =
+            response.StatusCode.GetValueOrDefault(StatusCodes.Status200OK);
+
+        context.Response.ContentType = response.ContentType;
+        context.Response.WriteAsync(response.Content ?? "unexpectedly empty");
     }
 }
